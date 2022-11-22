6b06f87298f175bbcac3783c8383ba908d8411b6
Cynto <lucagaravello@outlook.com>
2022-11-22 17:49:06 +0100

    Create scaffolding for HomeSection.tsx component

---
+++ app/(components)/HomeSection.tsx
@@ -0,0 +1,11 @@
+import styles from './(styles)/HomeSection.module.scss';
+
+const HomeSection = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <h1>Home Page </h1>
+    </div>
+  );
+};
+
+export default HomeSection;
