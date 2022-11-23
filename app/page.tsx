1de582c110aa78543b0960a98830887ed4286c69
Cynto <lucagaravello@outlook.com>
2022-11-23 15:32:00 +0100

    Add Navbar, AboutSection and ContactSection components to FullPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import HomeSection from './(components)/HomeSection';
+import Navbar from './(components)/Navbar';
+import AboutSection from './(components)/AboutSection';
+import ContactSection from './(components)/ContactSection';
 
 export default function FullPage() {
   return (
     <div>
       <HomeSection />
+      <div>
+        <Navbar />
+        <AboutSection />
+        <ContactSection />
+      </div>
     </div>
   );
 }
