1167ff60a37a8046aa0979e50a03efee78d241df
Cynto <lucagaravello@outlook.com>
2022-11-23 15:28:29 +0100

    Create scaffolding for AboutSection and ContactSection components

---
+++ app/(components)/AboutSection.tsx
@@ -0,0 +1,9 @@
+import styles from './(styles)/AboutSection.module.scss';
+
+export default function AboutSection() {
+  return (
+    <div className={styles.aboutContainer}>
+      <h1>About Page </h1>
+    </div>
+  );
+}
---
+++ app/(components)/ContactSection.tsx
@@ -0,0 +1,9 @@
+import styles from './(styles)/ContactSection.module.scss';
+
+export default function ContactSection() {
+  return (
+    <div className={styles.contactContainer}>
+      <h1>Contact Page </h1>
+    </div>
+  );
+}
