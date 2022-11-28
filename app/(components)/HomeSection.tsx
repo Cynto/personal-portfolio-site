c464a85e3c83dc0aa3713d35a452d5bb803c7ace
Cynto <lucagaravello@outlook.com>
2022-11-28 16:04:02 +0100

    Import Alegreya and Cuprum fonts, Add actual content to landing screen

--- app/(components)/HomeSection.tsx
+++ app/(components)/HomeSection.tsx
@@ -1,10 +1,34 @@
 import styles from './(styles)/HomeSection.module.scss';
+import { Alegreya, Cuprum } from '@next/font/google';
+import HomeBackground from './HomeBackground';
+
+const cuprum = Cuprum({
+  subsets: ['latin'],
+});
+const alegreya = Alegreya({
+  subsets: ['latin'],
+});
 
 const HomeSection = () => {
   return (
-    <div className={styles.homeContainer}>
-      <h1>Home Page </h1>
-    </div>
+    <>
+      <HomeBackground />
+      <div className={styles.homeContainer}>
+        <h2 className={alegreya.className}>
+          Hello there, I&apos;m <span>Luca Garavello</span>. <br />
+          I&apos;m a full-stack web developer.
+        </h2>
+        <button type={'button'} className={cuprum.className}>
+          <span>View my portfolio </span>
+          <svg viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
   );
 };
 
