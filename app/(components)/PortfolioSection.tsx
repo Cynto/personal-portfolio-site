1b544100d7d8984382a7abad3846822c21ef5274
Cynto <lucagaravello@outlook.com>
2022-11-28 17:48:43 +0100

    Create PortfolioSection component and scss scaffolding

---
+++ app/(components)/PortfolioSection.tsx
@@ -0,0 +1,9 @@
+import styles from './(styles)/PortfolioSection.module.scss';
+
+export default function PortfolioSection() {
+  return (
+    <section className={styles.portfolioSection} id={'portfolio'}>
+      <h2>Portfolio</h2>
+    </section>
+  );
+}
