939fbe7a63c0798453e647c664b8e711b2d94eea
Cynto <lucagaravello@outlook.com>
2022-11-28 19:31:32 +0100

    Add active class to the section that the user is currently on.

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { useEffect, useState } from 'react';
 import styles from './(styles)/Navbar.module.scss';
 import { Cuprum } from '@next/font/google';
 import { Squash as Hamburger } from 'hamburger-react';
@@ -9,10 +9,40 @@ const cuprum = Cuprum();
 
 export default function Navbar() {
   const { orientation } = useWindowProperties();
+  const [currentlyActive, setCurrentlyActive] = useState({
+    home: true,
+    about: false,
+    portfolio: false,
+    contact: false,
+  });
+
   const homeElement = document.getElementById('home');
   const aboutElement = document.getElementById('about');
   const portfolioElement = document.getElementById('portfolio');
   const contactElement = document.getElementById('contact');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (homeElement && aboutElement && portfolioElement && contactElement) {
+        setCurrentlyActive({
+          home:
+            homeElement.getBoundingClientRect().top < 100 &&
+            homeElement.getBoundingClientRect().bottom > 100,
+          about:
+            aboutElement.getBoundingClientRect().top < 100 &&
+            aboutElement.getBoundingClientRect().bottom > 100,
+          portfolio:
+            portfolioElement.getBoundingClientRect().top < 100 &&
+            portfolioElement.getBoundingClientRect().bottom > 100,
+          contact:
+            contactElement.getBoundingClientRect().top < 100 &&
+            contactElement.getBoundingClientRect().bottom > 100,
+        });
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [homeElement, aboutElement, portfolioElement, contactElement]);
 
   return (
     <nav className={styles.navbar}>
@@ -22,6 +52,7 @@ export default function Navbar() {
         <ul className={cuprum.className}>
           <li>
             <button
+              className={currentlyActive.home ? styles.active : ''}
               role="button"
               onClick={() => {
                 if (homeElement) {
@@ -34,6 +65,7 @@ export default function Navbar() {
           </li>
           <li>
             <button
+              className={currentlyActive.about ? styles.active : ''}
               role="button"
               onClick={() => {
                 if (aboutElement) {
@@ -46,6 +78,7 @@ export default function Navbar() {
           </li>
           <li>
             <button
+              className={currentlyActive.portfolio ? styles.active : ''}
               role="button"
               onClick={() => {
                 if (portfolioElement) {
@@ -58,6 +91,7 @@ export default function Navbar() {
           </li>
           <li>
             <button
+              className={currentlyActive.contact ? styles.active : ''}
               role="button"
               onClick={() => {
                 if (contactElement) {
