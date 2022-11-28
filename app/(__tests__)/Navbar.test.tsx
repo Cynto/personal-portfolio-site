ad9d5715f8bc17f21b76dfe32bd8f29d474c346e
Cynto <lucagaravello@outlook.com>
2022-11-28 15:54:26 +0100

    Update tests so getByText uses uppercase words. Add test to check hamburger menu is rendered if screen is portrait

--- app/(__tests__)/Navbar.test.tsx
+++ app/(__tests__)/Navbar.test.tsx
@@ -1,34 +1,35 @@
 import Navbar from '../(components)/Navbar';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 
 describe('Navbar', () => {
-  it('should render without crashing', () => {
-    render(<Navbar />);
+  beforeAll(() => {});
+  it('should render without crashing', async () => {
+    await render(<Navbar />);
 
     expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 
-  it('should render a list of 4 links', () => {
-    render(<Navbar />);
+  it('should render a list of 4 links', async () => {
+    await render(<Navbar />);
 
     expect(screen.getByRole('list')).toBeInTheDocument();
     expect(screen.getAllByRole('listitem')).toHaveLength(4);
     expect(screen.getAllByRole('link')).toHaveLength(4);
-    expect(screen.getByText('Home')).toBeInTheDocument();
-    expect(screen.getByText('Portfolio')).toBeInTheDocument();
-    expect(screen.getByText('About')).toBeInTheDocument();
-    expect(screen.getByText('Contact')).toBeInTheDocument();
+    expect(screen.getByText('HOME')).toBeInTheDocument();
+    expect(screen.getByText('PORTFOLIO')).toBeInTheDocument();
+    expect(screen.getByText('ABOUT')).toBeInTheDocument();
+    expect(screen.getByText('CONTACT')).toBeInTheDocument();
   });
 
   it('should navigate to the correct section when a link is clicked', async () => {
-    render(<Navbar />);
+    await render(<Navbar />);
 
-    const homeLink = screen.getByText('Home');
-    const aboutLink = screen.getByText('About');
-    const portfolioLink = screen.getByText('Portfolio');
-    const contactLink = screen.getByText('Contact');
+    const homeLink = screen.getByText('HOME');
+    const aboutLink = screen.getByText('ABOUT');
+    const portfolioLink = screen.getByText('PORTFOLIO');
+    const contactLink = screen.getByText('CONTACT');
 
     await userEvent.click(homeLink);
 
@@ -45,5 +46,20 @@ describe('Navbar', () => {
     await userEvent.click(contactLink);
 
     expect(window.location.hash).toBe('#contact');
+  });
+  it('should render a hamburger menu instead of a ul if the screen is portrait', async () => {
+    await render(<Navbar />);
+
+    await act(async () => {
+      Object.defineProperty(window, 'innerHeight', {
+        writable: true,
+        configurable: true,
+        value: 2000,
+      });
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
   });
 });
