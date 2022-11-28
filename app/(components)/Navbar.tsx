bc8f55ebea07033daff882aa48398c4c8bf36f7f
Cynto <lucagaravello@outlook.com>
2022-11-28 15:52:12 +0100

    Add hamburger menu component, use useWindowProperties hook to get window orientation. Add conditional rendering so hamburger is only rendered on portrait windows

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -3,26 +3,36 @@
 import styles from './(styles)/Navbar.module.scss';
 import { Cuprum } from '@next/font/google';
 import Link from 'next/link';
+import { Squash as Hamburger } from 'hamburger-react';
+import useWindowProperties from '../../hooks/useWindowProperties';
 
-const cuprum = Cuprum();
+const cuprum = Cuprum({
+  subsets: ['latin'],
+});
 
 export default function Navbar() {
+  const { orientation } = useWindowProperties();
+
   return (
     <nav className={styles.navbar}>
-      <ul className={cuprum.className}>
-        <li>
-          <Link href={'/#home'}>HOME</Link>{' '}
-        </li>
-        <li>
-          <Link href={'/#about'}>ABOUT</Link>
-        </li>
-        <li>
-          <Link href={'/#portfolio'}>PORTFOLIO</Link>
-        </li>
-        <li>
-          <Link href={'/#contact'}>CONTACT</Link>
-        </li>
-      </ul>
+      {orientation === 'portrait' ? (
+        <Hamburger size={40} />
+      ) : (
+        <ul className={cuprum.className}>
+          <li>
+            <Link href={'/#home'}>HOME</Link>{' '}
+          </li>
+          <li>
+            <Link href={'/#about'}>ABOUT</Link>
+          </li>
+          <li>
+            <Link href={'/#portfolio'}>PORTFOLIO</Link>
+          </li>
+          <li>
+            <Link href={'/#contact'}>CONTACT</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
