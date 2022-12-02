19a1e13c8940f6a728682cb8547e1533835b5dd2
Cynto <lucagaravello@outlook.com>
2022-12-02 16:21:17 +0100

    Create basic structure for about section. Need to add image

--- app/(components)/AboutSection.tsx
+++ app/(components)/AboutSection.tsx
@@ -1,9 +1,26 @@
 import styles from './(styles)/AboutSection.module.scss';
+import { Alegreya, Cuprum } from '@next/font/google';
+import Image from 'next/image';
+
+const cuprum = Cuprum();
+const alegreya = Alegreya();
 
 export default function AboutSection() {
   return (
-    <div className={styles.aboutContainer} id="about">
-      <h1>About Page </h1>
-    </div>
+    <section className={styles.aboutContainer} id="about">
+      <h2 className={alegreya.className}>ABOUT </h2>
+      <div className={styles.aboutMe}>
+        <h3 className={cuprum.className}>A little bit about me</h3>
+
+        <p>
+          I am a full stack developer with a passion for creating beautiful,
+          user friendly, fast, and secure web applications.
+        </p>
+        <p>
+          I prefer working with the backend, but I am very comfortable and
+          capable at working with the frontend as well.
+        </p>
+      </div>
+    </section>
   );
 }
