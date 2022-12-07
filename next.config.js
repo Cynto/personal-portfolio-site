9d36a7d6a4d591a8a2491839b482b4da8b96eb57
Cynto <lucagaravello@outlook.com>
2022-12-07 16:42:44 +0100

    Add more remote patterns

--- next.config.js
+++ next.config.js
@@ -20,6 +20,41 @@ const nextConfig = {
         hostname: 'upload.wikimedia.org',
         pathname: '/wikipedia/commons/thumb/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.iconscout.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'expressjs.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pngfind.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn4.iconfinder.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'git-scm.com',
+        pathname: '/**',
+      },
     ],
   },
 };
