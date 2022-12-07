e8ee88e016b9d6c8fe2985cac19f4dc3acbd81f4
Cynto <lucagaravello@outlook.com>
2022-12-07 16:51:09 +0100

    Create SkillsSection component, featuring frontend, backend, and misc skills

---
+++ app/(components)/SkillsSection.tsx
@@ -0,0 +1,103 @@
+import styles from './(styles)/SkillsSection.module.scss';
+import Image from 'next/image';
+import { Cuprum } from '@next/font/google';
+
+const cuprum = Cuprum();
+
+async function getSkills() {
+  try {
+    const res = await fetch(
+      'http://127.0.0.1:8090/api/collections/skills/records'
+    );
+
+    return await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default async function SkillsSection() {
+  const skillsData = await getSkills();
+  const skills = skillsData?.items ? skillsData.items : [];
+
+  return (
+    <section className={styles.skillsSectionContainer} id="skills">
+      <h2 className={cuprum.className}>Skills</h2>
+      <div className={styles.frontAndBackContainer}>
+        <div>
+          <h3 className={cuprum.className}>Frontend</h3>
+          <div className={styles.skillsContainer}>
+            {skills[0] &&
+              skills.map((skill: any) => {
+                if (skill.category === 'frontend') {
+                  return (
+                    <div className={styles.skill} key={skill.id}>
+                      {skill.name}
+                      <div className={styles.imageContainer}>
+                        <Image
+                          src={skill.icon}
+                          alt={skill.name}
+                          width={0}
+                          height={0}
+                          sizes="50px"
+                          title={skill.name}
+                        />
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+          </div>
+        </div>
+        <div>
+          <h3 className={cuprum.className}>Backend</h3>
+          <div className={styles.skillsContainer}>
+            {skills.map((skill: any) => {
+              if (skill.category === 'backend') {
+                return (
+                  <div className={styles.skill} key={skill.id}>
+                    {skill.name}
+                    <div className={styles.imageContainer}>
+                      <Image
+                        src={skill.icon}
+                        alt={skill.name}
+                        width={0}
+                        height={0}
+                        sizes="50px"
+                        title={skill.name}
+                      />
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+      <div className={styles.miscContainer}>
+        <h3 className={cuprum.className}>Miscellaneous</h3>
+        <div className={styles.skillsContainer}>
+          {skills.map((skill: any) => {
+            if (skill.category === 'misc') {
+              return (
+                <div className={styles.skill} key={skill.id}>
+                  {skill.name}
+                  <div className={styles.imageContainer}>
+                    <Image
+                      src={skill.icon}
+                      alt={skill.name}
+                      width={0}
+                      height={0}
+                      sizes="50px"
+                      title={skill.name}
+                    />
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
