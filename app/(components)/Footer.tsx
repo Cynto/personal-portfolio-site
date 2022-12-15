c0045172e6d760005fc079b09c14e39bef0cb205
Cynto <lucagaravello@outlook.com>
2022-12-15 17:46:12 +0100

    Add styles import

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,6 +1,7 @@
+import styles from './(styles)/Footer.module.scss';
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div></div>
     </footer>
   );
