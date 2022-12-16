8da422e46f10233edd00aa71f1dc6124977bb182
Cynto <lucagaravello@outlook.com>
2022-12-16 18:07:36 +0100

    Update title, add meta description, add icon for site

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,15 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
+        <title>Luca Garavello</title>
+        <meta
+          name="description"
+          content="Luca Garavello's personal website. Full-stack web developer."
+        />
+        <link
+          rel={'icon'}
+          href={'https://cdn-icons-png.flaticon.com/512/1666/1666094.png'}
+        />
       </head>
       <body>{children}</body>
     </html>
