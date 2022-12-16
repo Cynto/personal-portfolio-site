ae44f4f628780b5b0adc87a24e67d564a75fe0cc
Cynto <lucagaravello@outlook.com>
2022-12-16 17:41:38 +0100

    Add LinkedIn, GitHub link, add name with copyright notice

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,8 +1,28 @@
 import styles from './(styles)/Footer.module.scss';
+import { Cuprum } from '@next/font/google';
+
+const cuprum = Cuprum();
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div></div>
+      <div className={styles.socialsContainer}>
+        <div className={styles.social}>
+          <a
+            href="https://www.linkedin.com/in/luca-garavello0/"
+            title="Linkedin"
+          >
+            <i className="devicon-linkedin-plain"></i>
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a href="https://github.com/Cynto" title="GitHub">
+            <i className="devicon-github-original"></i>
+          </a>
+        </div>
+      </div>
+      <p className={cuprum.className}>
+        Luca Garavello <span>&copy;{new Date().getFullYear()}</span>
+      </p>
     </footer>
   );
 }
