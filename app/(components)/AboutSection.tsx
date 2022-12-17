4d5c6a0fbe93b4ea82945bc2872bfb3617769076
Cynto <lucagaravello@outlook.com>
2022-12-17 15:23:27 +0100

    Add image of me, change second paragraph

--- app/(components)/AboutSection.tsx
+++ app/(components)/AboutSection.tsx
@@ -1,5 +1,6 @@
 import styles from './(styles)/AboutSection.module.scss';
 import { Alegreya, Cuprum } from '@next/font/google';
+import photoOfMe from '../../public/photo_of_me.jpg';
 import Image from 'next/image';
 
 const cuprum = Cuprum();
@@ -10,6 +11,7 @@ export default function AboutSection() {
     <section className={styles.aboutContainer} id="about">
       <h2 className={alegreya.className}>ABOUT </h2>
       <div className={styles.aboutMe}>
+        <Image src={photoOfMe} alt="Luca Garavello" />
         <h3 className={cuprum.className}>A little bit about me</h3>
 
         <p data-testid="para">
@@ -17,8 +19,12 @@ export default function AboutSection() {
           user friendly, fast, and secure web applications.
         </p>
         <p data-testid="para">
-          I prefer working with the backend, but I am very comfortable and
-          capable at working with the frontend as well.
+          Although I am full stack developer, my passion lies more within the
+          backend side of developing. This however, does not mean I&apos;m
+          uncomfortable at working on the frontend side of developing. I
+          thoroughly enjoy working with the latest technology to implement APIs
+          and databases. Additionally, I&apos;m a big fan of clean code and
+          I&apos;m always looking to learn further ways in improving my code.
         </p>
       </div>
     </section>
