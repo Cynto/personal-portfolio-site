d7093383e21d746ff1dba71ae9ad0081a21a56a8
Cynto <lucagaravello@outlook.com>
2022-12-20 17:49:31 +0100

    Move ProjectsContainer into its own component. Replace screenshot url for each project with actual url, instead of its previous file name. Make sure that the featured project is second in projects array

--- app/(components)/PortfolioSection.tsx
+++ app/(components)/PortfolioSection.tsx
@@ -1,9 +1,9 @@
 import styles from './(styles)/PortfolioSection.module.scss';
-import { Alegreya, Cuprum } from '@next/font/google';
+import { Alegreya } from '@next/font/google';
 import AuthData from '../(interfaces)/AuthData.interface';
-import Image from 'next/image';
+import ProjectsContainer from './ProjectsContainer';
 
-const cuprum = Cuprum();
+
 const alegreya = Alegreya();
 
 const getProjects = async (authData: AuthData) => {
@@ -18,8 +18,19 @@ const getProjects = async (authData: AuthData) => {
         },
       }
     );
+    const data = await res.json();
+    const projects = data?.items ? data.items.map((project: any) => {
+      return {
+        ...project,
+        screenshot: project.screenshot !== '' ? `${process.env.DB_HOST}/api/files/${project.collectionName}/${project.id}/${project.screenshot}` : ''
+      }
+    }) : [];
 
-    return await res.json();
+    const featuredIndex = projects.findIndex((project: any) => project.featured === true);
+    const secondProject = projects[1];
+    projects[1] = projects[featuredIndex];
+    projects[featuredIndex] = secondProject;
+    return await projects;
   } catch (err) {
     console.log(err);
   }
@@ -29,59 +40,11 @@ export default async function PortfolioSection({
 }: {
   authData: AuthData;
 }) {
-  const projectsData = await getProjects(authData);
-  const projects = projectsData?.items ? projectsData.items : [];
-
-  console.log(projects);
+  const projects = await getProjects(authData);
   return (
     <section className={styles.portfolioSection} id={'portfolio'}>
       <h2 className={alegreya.className}>Portfolio</h2>
-
-      <div className={styles.projectsContainer}>
-        {projects[0] &&
-          projects.map((project: any) => {
-            return (
-              <div className={styles.project} key={project.id}>
-                <div className={styles.imageContainer}>
-                  {project.screenshot !== '' && (
-                    <Image
-                      src={`${process.env.DB_HOST}/api/files/${project.collectionName}/${project.id}/${project.screenshot}`}
-                      alt={project.name}
-                      quality={100}
-                      sizes={' 100vw, 50vw'}
-                      fill={true}
-                      style={{ objectFit: 'cover' }}
-                    />
-                  )}
-                  <div
-                    className={styles.overlay}
-                    style={{
-                      opacity: project.screenshot === '' ? 1 : 0,
-                    }}
-                  >
-                    <div className={styles.overlayText}>
-                      <h3 className={cuprum.className}>{project.name}</h3>
-                      <p className={cuprum.className}>
-                        {project.expand.built_with.map(
-                          (tech: any, index: number) => {
-                            return (
-                              <span key={tech.id}>
-                                {index !== 0 ? ' / ' : ''} {tech.name}
-                              </span>
-                            );
-                          }
-                        )}
-                      </p>
-                    </div>
-                    <div className={styles.overlayLinks}>
-                      <a href={project.github_repository}></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
+      <ProjectsContainer projects={projects} />
     </section>
   );
 }
