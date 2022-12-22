10c7a5844aa63ef37ee1798cecac2459379cfa79
Cynto <lucagaravello@outlook.com>
2022-12-22 16:29:30 +0100

    Move SwitchProjects container into own component

--- app/(components)/ProjectsContainer.tsx
+++ app/(components)/ProjectsContainer.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { Cuprum } from '@next/font/google';
 import { useState } from 'react';
 import Project from '../(interfaces)/Project.interface';
+import SwitchProjectButtons from './SwitchProjectButtons';
 
 const cuprum = Cuprum();
 export default function ProjectsContainer({
@@ -61,40 +62,10 @@ export default function ProjectsContainer({
             );
           })}
       </div>
-      <div className={styles.switchProjectButtons}>
-        <button
-          className={styles.switchProjectLeft}
-          onClick={() => {
-            if (projectsArr[0] !== undefined) {
-              const projectsArrCopy = [...projectsArr];
-              const lastProject = projectsArrCopy.pop();
-              if (lastProject) {
-                projectsArrCopy.unshift(lastProject);
-                setProjectsArr(projectsArrCopy);
-              }
-            }
-          }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-            <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-          </svg>
-        </button>
-        <button
-          className={styles.switchProjectRight}
-          onClick={() => {
-            const projectsArrCopy = [...projectsArr];
-            const firstProject = projectsArrCopy.shift();
-            if (firstProject) {
-              projectsArrCopy.push(firstProject);
-              setProjectsArr(projectsArrCopy);
-            }
-          }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-            <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-          </svg>
-        </button>
-      </div>
+      <SwitchProjectButtons
+        projectsArr={projectsArr}
+        setProjectsArr={setProjectsArr}
+      />
     </>
   );
 }
